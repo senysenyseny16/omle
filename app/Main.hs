7d9613228b4c2fdd00beac872ee6e2dbc9b58be4
Arseny <arseny@protonmail.com>
2025-03-19 09:55:27 +0700

    feat: argparser

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,24 @@
 module Main (main) where
 
-import Lib
-import Omle.AST
-import Omle.Parser
+import Omle.Parser (parseScalar)
+import Options.Applicative
+import Text.Megaparsec
+
+data Input = Input
+  {inputFile :: FilePath}
+  deriving (Show)
+
+input :: Parser Input
+input = Input <$> strArgument (metavar "INPUT_FILE" <> help "File to parse")
+
+yamlParse :: Input -> IO ()
+yamlParse Input {inputFile = inputFile'} = do
+  content <- readFile inputFile'
+  case parse parseScalar "" content of
+    Left err -> putStrLn $ "error: " ++ show err
+    Right ast -> print ast
 
 main :: IO ()
-main = someFunc
+main = yamlParse =<< execParser opts
+  where
+    opts = info (input <**> helper) (fullDesc <> progDesc "YAML formatter and linter")
