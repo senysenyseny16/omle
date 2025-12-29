7c9498655b00a5efcd1e43693de7f56e898dd008
dulepova <nasty731@gmail.com>
2025-12-29 16:54:44 +0100

    test: expand YAML value tests to include strings and null
    Add plain and quoted string scalars, as well as null values,
    to parseYamlValue tests in block and flow contexts.

--- test/Parser/YamlValueSpec.hs
+++ test/Parser/YamlValueSpec.hs
@@ -15,37 +15,76 @@ spec = do
       parse
         parseSequence
         ""
-        "[32.456 ,  false, [7, true],  1 ]"
+        "[32.456 ,  false, [7, hello, true],  1, NULL]"
         `shouldBe` Right
           ( YamlSequence
-              [ YamlScalar (YamlFloat 32.456),
-                YamlScalar (YamlBool False),
-                YamlSequence [YamlScalar (YamlInt 7), YamlScalar (YamlBool True)],
-                YamlScalar (YamlInt 1)
+              [ YamlScalar (YamlFloat 32.456)
+              , YamlScalar (YamlBool False)
+              , YamlSequence
+                  [ YamlScalar (YamlInt 7)
+                  , YamlScalar (YamlString "hello")
+                  , YamlScalar (YamlBool True)
+                  ]
+              , YamlScalar (YamlInt 1)
+              , YamlScalar YamlNull
               ]
           )
 
   describe "parseMapping" $ do
-    it "parses nested mappings" $ do
+    it "parses nested mappings with mixed scalar types" $ do
       parse
         parseMapping
         ""
-        "{key: {key1: [44, false]}}"
+        "{key: {key1: [44, false, hello, \"world\", null]}}"
         `shouldBe` Right
-          (YamlMapping [("key", YamlMapping [("key1", YamlSequence [YamlScalar (YamlInt 44), YamlScalar (YamlBool False)])])])
+          (YamlMapping
+            [ ("key"
+              , YamlMapping
+                  [ ("key1"
+                    , YamlSequence
+                        [ YamlScalar (YamlInt 44)
+                        , YamlScalar (YamlBool False)
+                        , YamlScalar (YamlString "hello")
+                        , YamlScalar (YamlString "world")
+                        , YamlScalar YamlNull
+                        ]
+                    )
+                  ]
+              )
+            ])
 
   describe "parseYamlValue" $ do
-    it "parses structures with indentation" $ do
+    it "parses structures with indentation including strings" $ do
       parse
         parseYamlValue
         ""
         (T.unlines
-        [ "k1 :"
-        , "  k11 : "
-        , "    - 1"
-        , "    - 2"
-        , "  k12 :"
-        , "    k121 : [true,1]"
-        ])
+          [ "k1 :"
+          , "  k11 : "
+          , "    - 1"
+          , "    - 2"
+          , "    - hello"        -- plain string
+          , "    - \"world\""    -- quoted string
+          , "  k12 :"
+          , "    k121 : [true, 1, foo, ~]"  -- flow sequence with plain string and null
+          ])
         `shouldBe` Right
-          (YamlMapping [("k1",YamlMapping [("k11",YamlSequence [YamlScalar (YamlInt 1),YamlScalar (YamlInt 2)]),("k12",YamlMapping [("k121",YamlSequence [YamlScalar (YamlBool True),YamlScalar (YamlInt 1)])])])])
+          (YamlMapping
+            [ ("k1"
+              , YamlMapping
+                [ ("k11", YamlSequence
+                    [ YamlScalar (YamlInt 1)
+                    , YamlScalar (YamlInt 2)
+                    , YamlScalar (YamlString "hello")
+                    , YamlScalar (YamlString "world")
+                    ])
+                , ("k12", YamlMapping
+                    [ ("k121", YamlSequence
+                        [ YamlScalar (YamlBool True)
+                        , YamlScalar (YamlInt 1)
+                        , YamlScalar (YamlString "foo")
+                        , YamlScalar YamlNull
+                        ])
+                    ])
+                ])
+            ])
